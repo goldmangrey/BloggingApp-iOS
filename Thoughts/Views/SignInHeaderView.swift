4bcf7c90ad13bde8c4e5bc5c806136311018b742
Yeskendir <91181514+goldmangrey@users.noreply.github.com>
2022-05-23 05:08:38 +0600

    Add files via upload

---
+++ Thoughts/Views/SignInHeaderView.swift
@@ -0,0 +1,45 @@
+//
+//  SignInHeaderView.swift
+//  Thoughts
+//
+//  Created by Afraz Siddiqui on 7/25/21.
+//
+
+import UIKit
+
+class SignInHeaderView: UIView {
+
+    private let imageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "logo"))
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    private let label: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 20, weight: .medium)
+        label.text = "Blogging"
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        clipsToBounds = true
+        addSubview(label)
+        addSubview(imageView)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        let size: CGFloat = width/4
+        imageView.frame = CGRect(x: (width-size)/2, y: 10, width: size, height: size)
+        label.frame = CGRect(x: 20, y: imageView.bottom+10, width: width-40, height: height-size-30)
+    }
+}
